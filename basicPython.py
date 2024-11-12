5615f2e84276118d1c9f6c66ec6814a4f4fb9938
vasanthsubram <vasanth@ip-192-168-68-53.us-west-2.compute.internal>
2024-11-12 11:39:11 -0800

    comments

--- basicPython.py
+++ basicPython.py
@@ -27,3 +27,24 @@ print(helloworld)
 
 a, b = 3,4
 print(a,b)
+
+#print number and string
+name = "John"
+age = 30
+#using concatnation
+print('using concatenation')
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+#using f string
+print("using f string")
+print(f"My name is {name} and I am {age} years old.")
+
+#using formatted string
+print("using formatted string")
+message = "My name is {} and I am {} years old."
+formatted_message = message.format(name, age)
+print(formatted_message)
+
+#using join()
+print("using join")
+print("My name is", name, "and I am", age, "years old.")
